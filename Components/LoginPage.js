4a35536ebc1dcf055e6c99470c8977a2653b6131
Rayyan-H <77022512+Rayyan-H@users.noreply.github.com>
2021-10-25 17:35:14 -0400

    Add files via upload
    Right now atm, there isn't a way to switch between the components of the login or register without altering it in the code.

---
+++ Components/LoginPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PageTitle from "../components/PageTitle";
+import Login from "../components/Register";
+import "../css/style.css";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <PageTitle />
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ Components/PageTitle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PageTitle() {
+  return <h1 id="title">Welcome to Yu-Gi-Oh Duel Links!</h1>;
+}
+
+export default PageTitle;
