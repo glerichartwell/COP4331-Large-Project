5369de2dfb7bbeb02b5427b266a03ec41e9f1218
Shane Davis <ruunanaa@gmail.com>
2021-11-03 17:44:32 -0400

    Added fonts (probably can remove the ones we aren't using at the end). Added colors from palette (needs descriptive names).

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import colors from './assets/colors/colors';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'Montserrat-Black': require('./assets/fonts/Montserrat-Black.ttf'),
+    'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+    'WorkSans-Black': require('./assets/fonts/WorkSans-Black.ttf')
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: 'Montserrat-Bold'}}>Open up App.js to start working on your app! cur test12345WORK</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.main,
     alignItems: 'center',
     justifyContent: 'center',
   },
