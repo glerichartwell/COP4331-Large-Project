b8abf8e2395a4d5d7e63897a40c7200ca6e5f941
glerichartwell <75506480+glerichartwell@users.noreply.github.com>
2021-11-04 13:01:36 -0400

    Shane Mobile Merge

---
+++ mobile/assets/colors/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    main: '#64104D',
+    main2: '#ED5E4F',
+    color1: '#B21B3F',
+    color2: '#D2C3E4',
+    color3: '#EB3248',
+    color4: '#28B7CB'
+}
+
+export default colors;
