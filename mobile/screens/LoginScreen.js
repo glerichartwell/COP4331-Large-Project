e6264d86fbcc6cf5b67511f41f8249e4e10cf5dc
Shane Davis <ruunanaa@gmail.com>
2021-11-18 04:03:27 -0500

    Progress on the login screen.

--- mobile/screens/LoginScreen.js
+++ mobile/screens/LoginScreen.js
@@ -1,12 +1,52 @@
 import * as React from 'react';
 import {ImageBackground, Platform, StyleSheet, Text} from 'react-native';
-import {Surface, Title} from 'react-native-paper';
+import {Button, Subheading, Surface, TextInput, Title} from 'react-native-paper';
+import theme from '../custom-properties/Themes'
 
 const Screen = () => {
+    const [login, setLogin] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    /*let state = {
+        icon: "eye",
+        password: true
+    };*/
+    let passwordIcon = "eye";
+    let showPassword = false;
+
+    function changeIcon() {
+        passwordIcon = (passwordIcon = "eye") ? "eye-off" : "eye";
+        showPassword = !showPassword;
+    }
+
     return (
         <ImageBackground source={require('../assets/images/palette_max.jpg')} style={styles.backgroundImage}>
             <Title style={styles.title1}>Hello again!</Title>
             <Title style={styles.title2}>Welcome back</Title>
+            <Surface style={styles.surface}>
+                <Subheading style={styles.loginText}>Login</Subheading>
+                <TextInput
+                    style={styles.textInput}
+                    mode="outlined"
+                    label="E-mail"
+                    /*right={<TextInput.Icon name="check" />}*/
+
+                    value={login}
+                    onChangeText={login => setLogin(login)}
+                />
+                <TextInput
+                    style={styles.textInput}
+                    mode="outlined"
+                    label="Pass-word"
+                    /*right={<TextInput.Icon name={passwordIcon} onPress={() => changeIcon()}/>}*/
+
+                    secureTextEntry={!showPassword}
+                    value={password}
+                    onChangeText={password => setPassword(password)}
+                />
+                <Button mode="contained" style={styles.submitButton}>
+                    Submit
+                </Button>
+            </Surface>
         </ImageBackground>
     );
 }
@@ -14,48 +54,39 @@ const Screen = () => {
 export default Screen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        //alignItems: 'center',
-        //justifyContent: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0)'
-    },
-    backgroundImage: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-        // justifyContent: "center",
-        // alignItems: "center",
-        opacity: 1,
-        backgroundColor: 'blue'
-    },
-    rectangle: {
-        height: '48%',
-        width: '100%',
-        backgroundColor: 'white',
-        position: 'absolute',
-        zIndex: 99,
-        top: '55%',
-        left: '0%',
-        borderRadius: 20,
-    },
-    title1:{
-        // height: 115,
-        // width: 360,
-        /*marginTop: 300,
-        backgroundColor: 'black',
-        marginLeft: 20,
-        fontSize: 48,
-        textAlign: "left",*/
-        paddingTop: 100,
-        paddingLeft: 20,
-        fontSize: 48,
-        color: "white",
-    },
-    title2:{
-        paddingTop: 20,
-        paddingLeft: 20,
-        fontSize: 48,
-        color: "white",
+        backgroundImage: {},
+        title1: {
+            paddingTop: 100,
+            paddingLeft: 20,
+            fontSize: 48,
+            color: "white",
+        },
+        title2: {
+            paddingTop: 20,
+            paddingLeft: 20,
+            fontSize: 48,
+            color: "white",
+        },
+        surface: {
+            backgroundColor: "white",
+            borderRadius: 20,
+            top: "30%",
+            height: "50%",
+            paddingLeft: 50,
+            paddingRight: 50,
+            paddingTop: 70
+        },
+        loginText: {
+            fontSize: 24,
+            paddingBottom: 10
+        },
+        textInput: {
+            width: "100%",
+            paddingBottom: 30
+        },
+        submitButton: {
+            borderRadius: 10,
+            backgroundColor: theme.colors.color4
+        },
     }
-});
+);
