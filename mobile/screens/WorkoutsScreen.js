246e005ec2133481fc666a3f5ff96446b3b873da
Shane Davis <ruunanaa@gmail.com>
2021-11-29 00:13:35 -0500

    Pushing before pull (starting workout screen).

--- mobile/screens/WorkoutsScreen.js
+++ mobile/screens/WorkoutsScreen.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Text} from "react-native-paper";
 import {getAuth, onAuthStateChanged} from "firebase/auth";
 import {StyleSheet, View} from "react-native";
 import TopBar from "../components/TopBar";
 
-const WorkoutsScreen = () => {
+const WorkoutsScreen = (props) => {
     const auth = getAuth();
-    let email;
+    const [workoutInfo, setWorkoutInfo] = useState(null);
+    const [date, setDate] = useState(new Date());
+    const [loaded, setLoaded] = useState(false);
 
-    onAuthStateChanged(auth, (user) => {
-        if (user != null){
-            email = user.email;
-        }
-        else {
-            email = "";
-        }
-    });
+    useEffect(() => {
+        loadWorkoutInfo();
+    }, [loaded])
+
+    const loadWorkoutInfo = () => {
+        console.log("-----------");
+        console.log("Loading workout info");
+        let js = JSON.stringify({email: props.email, startDate: date.toISOString()});
+        fetch("http://192.168.208.1:5000/api/view-client-workouts-by-week",
+            {
+                method: "POST",
+                body: js,
+                headers: {"Content-Type": "application/json"},
+            })
+            .then(response => response.json())
+            .then((responseJson) => {
+                console.log(responseJson)
+                setLoaded(true);
+            })
+            .catch(error => console.log("ERROR: " + error))
+        console.log("-----------");
+    }
 
     return (
         <View>
