91d944e31698a58076325c953a805cdb9e5d5119
glerichartwell <75506480+glerichartwell@users.noreply.github.com>
2021-12-01 18:59:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -179,5 +179,5 @@ app.use(express.static('build'));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/build/index.html'))
 })
-app.listen(5000); // start Node + Express server on port 4000
+app.listen(PORT); // start Node + Express server on port 4000
 console.log("listening", PORT);
