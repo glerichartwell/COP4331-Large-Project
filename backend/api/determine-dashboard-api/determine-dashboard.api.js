0b015c24f4821de4dab473bf862ab657d1191e29
glerichartwell <75506480+glerichartwell@users.noreply.github.com>
2021-12-02 14:22:44 -0500

    Update determine-dashboard.api.js

--- backend/api/determine-dashboard-api/determine-dashboard.api.js
+++ backend/api/determine-dashboard-api/determine-dashboard.api.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post("/api/determine-dashboard", async (req, res) => {
   // incoming: email
   // outgoing: true/false, or error
-  console.log("Incoming: ", email)
+
 
   var error = "";
   var isTrainer = false;
