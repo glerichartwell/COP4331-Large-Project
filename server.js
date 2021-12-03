36cea5fbd3c4813a229d41d9b86a53a4462a399d
glerichartwell <75506480+glerichartwell@users.noreply.github.com>
2021-12-03 05:09:10 -0500

    Update server.js
    He's done it again! Leave the comments where they're supposed to be...

--- server.js
+++ server.js
@@ -175,7 +175,7 @@ app.use((req, res, next) => {
 app.use(express.static('build'));
 
 
-AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/build/index.html'))
 })
